7752fc0690f6c794661d4d610d171162487ea82f
1 Maguire <157508059+1Maguire@users.noreply.github.com>
2024-03-20 19:42:58 -0500

    createDistance.kt

--- module04/week08/fallingDistance.kt
+++ module04/week08/fallingDistance.kt
@@ -1,3 +1,14 @@
 fun main(){
-d= 1/2(gt2
+
+    val g =9.8
+    var t = 1
+
+    var counter = 1
+    while (counter <= 10) {
+        var d = 0.5 * (g * (t * t))
+        println("At $t seconds the distance is $d.")
+        t++
+        counter++
+    }
+
 }
